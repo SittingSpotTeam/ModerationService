bfcef0d050d713b2f2f30c8bd5180fd9b5bd8eae
davipase <davipase@gmail.com>
2024-07-29 12:01:47 +0200

    add dockerfile - to finish

--- src/main/java/com/sittingspot/moderationservice/ModerationServiceApplication.java
+++ src/main/java/com/sittingspot/moderationservice/ModerationServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ModerationserviceApplication {
+public class ModerationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ModerationserviceApplication.class, args);
+		SpringApplication.run(ModerationServiceApplication.class, args);
 	}
 
 }
